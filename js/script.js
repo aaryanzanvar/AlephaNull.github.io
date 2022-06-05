6374385a4b43c962e34d0c75271f32ae40e65335
Alepha Null <HashTag>
2022-06-05 05:24:06 -0400

    fix: toggle btn, menuBtn, arrow

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ const sections = document.querySelectorAll("section[id]");
 window.addEventListener("scroll", navHighlighter);
 
 function navHighlighter() {
-  
+
   // Get current scroll position
   let scrollY = window.pageYOffset;
-  
+
   // Now we loop through sections to get height, top and ID values for each
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight;
-  
+
     // Original:
     // const sectionTop = current.offsetTop - 50;
     //  
@@ -24,7 +24,7 @@ function navHighlighter() {
     //
     const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 50;
     const sectionId = current.getAttribute("id");
-    
+
     /*
     - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
@@ -32,7 +32,7 @@ function navHighlighter() {
     if (
       scrollY > sectionTop &&
       scrollY <= sectionTop + sectionHeight
-    ){
+    ) {
       document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
     } else {
       document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
@@ -50,11 +50,13 @@ const nav = document.querySelector("nav");
 // listens for click events and when a click occurs a class is created see CSS
 hamburger.addEventListener('click', () => {
   nav.classList.add('open-nav');
+  hamburger.classList.add('spin');
 
 })
 
 close.addEventListener('click', () => {
   nav.classList.remove('open-nav');
+  hamburger.classList.remove('spin');
 
 })
 
@@ -70,7 +72,7 @@ const titleNorm = document.querySelector(".title-normal");
 
 togBtn.addEventListener("click", function() {
   if (togBtn.checked == true) {
-    /** descriptions **/ 
+    /** descriptions **/
     descNerd.classList.add("hide-text");
     descNormal.classList.remove("hide-text");
     /** titles **/
